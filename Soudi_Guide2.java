dfcde114cf4e25eabc8243069b5e44ee34388898
LeenAlsulami <90353695+LeenAlsulami@users.noreply.github.com>
2021-12-04 16:19:36 +0300

    Add files via upload

--- Soudi_Guide2.java
+++ Soudi_Guide2.java
@@ -3,56 +3,62 @@ package cpit251;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+public class Soudi_Guide2 {
 
-public class SaudiGuide {
+    public static void main(String[] args) {
 
-    public static void main(String[] args) {      
-        // Create ArrayList from Account class and name it accounts
+        //1) Create ArrayList from Account class and name it accounts
         ArrayList<Account> accounts = new ArrayList<>();
-        // Users 
+        //2) Users 
         Account user_1 = new Account("Sarah", "Naghi", "Sarh@hotmail.com", "0500000001", "Sarah", "Ss@12345678", 23);
         Account user_2 = new Account("Rawan", "Samer", "Rawan@hotmail.com", "0500000002", "RawanS", "Rr@12345678", 22);
         Account user_3 = new Account("Ragad", "Fahad", "Ragad@hotmail.com", "0500000003", "RagadFt", "Rr@12345678", 22);
         Account user_4 = new Account("Leen", "Saleh", "Leen@hotmail.com", "0500000004", "LeenS", "Ll@12345678", 23);
-        // Inserting users into the accounts ArrayList
+        //3) Inserting users into the accounts ArrayList
         accounts.add(user_1);
         accounts.add(user_2);
         accounts.add(user_3);
         accounts.add(user_4);
-        
-        /////// Creat Cities objects
 
+        //Scanner to get values from users
+        Scanner input = new Scanner(System.in);
 
-        //-------------------------------------------Main menu loop------------------------------------------//
+        // state of user 
+        boolean login = false;
+        int UserIndex;
+
+//-------------------------------------------Main menu loop------------------------------------------//  
         while (true) {
             System.out.println("\n\n---------------------------------");
             System.out.println("Welcome in saudi guide system");
             System.out.println("----------------------------------");
             System.out.println("Choose a number");
-            System.out.println("1- Log in");
-            System.out.println("2- Register");
-            Scanner input = new Scanner(System.in);
-            System.out.print("Enter 1 or 2 : ");
+            System.out.println("1- To log in");
+            System.out.println("2- To register");
+            System.out.println("3- To choose a city enter");
+
+            System.out.print("Enter a number: ");
             String number = input.next();
 
-            //-------------------------------------LogIn---------------------------------//
+            //-------------------------------------1) LogIn---------------------------------//          
             if (number.equals("1")) {
                 System.out.println("Log in page");
                 System.out.print("Enter username: ");
                 String username = input.next();
                 // Search for the entered username in the ArrayList 
-                int usernemeExists = Account.SearchByusername(accounts, username);
+                UserIndex = Account.SearchByusername(accounts, username);
                 while (true) {
 
                     // Username not found in the ArrayList
-                    if (usernemeExists == -1) {
+                    if (UserIndex == -1) {
                         System.out.println("This username dose not exists");
                         System.out.print("Please try again: ");
                         username = input.next();
-                        usernemeExists = Account.SearchByusername(accounts, username);
+                        UserIndex = Account.SearchByusername(accounts, username);
                         // Username found in the ArrayList    
                     } else {
                         break;
+
                     }
                 }
 
@@ -62,38 +68,21 @@ public class SaudiGuide {
                     // According to username, we'll use the index in order to validate the entered password 
                     // and match it with the one in the ArrayList
                     // Password did not match
-                    if (!accounts.get(usernemeExists).getPassword().equals(password)) {
+                    if (!accounts.get(UserIndex).getPassword().equals(password)) {
                         System.out.println("Sorry! Wrong password, please try again");
                         password = input.next();
                     }
-                    // Password matched
+                    System.out.println("\n\n      You’ve logged in successfully! ");
+                    login = true;
                     break;
                 }
+                break;
 
-                // VIEW this page when user Logged In
-                System.out.println("\n\n      You’ve logged in successfully! ");
-                System.out.println("      ————-to home page————");
-                System.out.println("     - To view account information enter(1)");
-                System.out.println("     - To view our menu enter(2)");
-                System.out.println("     - To choose a city enter(3)");
-                System.out.println("     - To Exit (4)");
-                System.out.print("     - Enter Your choice: ");
-                int choice = input.nextInt();
-                // To display user's account information
-                if (choice == 1) {
-                    accounts.get(usernemeExists).AccountInformation();
-                } // To display the menu
-                else if (choice == 2) {
-                    continue;
-
-                } else if (choice == 3) {
-                    Destination dis = new Destination();
-                    dis.DisplayInf();
-                } else if (choice == 4) {
-                    System.exit(0);
-                }
-
-                //------------------------------------Registeration----------------------------------//
+//--------------------------------- End Of LogIn--------------------------------------//  
+//*
+//*
+//*
+//------------------------------------Registeration----------------------------------//
             } else if (number.equals("2")) {
 
                 System.out.println("-------- Registeration page ------- ");
@@ -211,57 +200,53 @@ public class SaudiGuide {
                 ////////////////////////////////////////////////////////////////////////////////////////////////
                 // Create user's account
                 Account user = new Account(fname, lname, Email, Phone, username, password, age);
-                // Inserting users into the accounts ArrayList
+                // *****************Inserting users into the accounts ArrayList****************
                 accounts.add(user);
-                
-                
-                
-                // View menu
-                System.out.println("");
+                login = true;
+                UserIndex = Account.SearchByusername(accounts, username);
+
                 System.out.println("");
                 System.out.println("-----------------------------------------------------");
                 System.out.println("      Account has been successfully created!");
-                System.out.println("      —––––——<< Welcome to Saudi Guide >>—––––——");
-                System.out.println("      - To view account information enter(1)");
-                System.out.println("      - To view our menu enter(2)");
-                System.out.println("      - To choose city enter(3)");
-                System.out.println("-----------------------------------------------------");
+                break;
+            } //-----------------------------------------------End Of Registeration------------------------------------------// 
+            else if (number.equals("3")) {
+                Destination dis = new Destination();
+                dis.DisplayInf();
                 System.out.println("");
-                System.out.println("");
-                int choice = input.nextInt();
+                System.out.print("To home page enter (1)  --->");
+                int numberTo = input.nextInt();
 
-                // To display account information
-                if (choice == 1) {
-                    user.AccountInformation();
-                } else if (choice == 2) {
-                    continue;
-                } else if (choice == 3) {
-                    Destination dis = new Destination();
-                    dis.DisplayInf();
-                }
-
-                System.out.println("      - To home page enter(1)");
-                System.out.println("      - To Exit enter(3)");
-                System.out.print("--->");
-                choice = input.nextInt();
-
-                if (choice == 1) {
-                    continue;
-                } // To display exit page and terminate the system
-                else if (choice == 3) {
-                    System.out.println("*****!!! Thank you for using Saudi Guide !!!*****");
-                }
-                System.out.println("           We hope you found the perfect spot you wanted <3 ");
-                System.out.println("                                SEE YA");
-                System.exit(0);
-
-                //-----------------------------------------------Wrong Choice------------------------------------------// 
             } else {
                 System.out.print("wrong input, please enter 1 or 2: ");
                 number = input.next();
             }
-
         }
 
+        while (login) {
+            System.out.println("      —––––——<< Welcome to Saudi Guide >>—––––——");
+            System.out.println("      - To view account information enter(1)");
+            System.out.println("      - To choose city enter(2)");
+            System.out.println("      - To Exit (3)");
+            System.out.println("-----------------------------------------------------");
+            System.out.println("");
+            System.out.println("");
+            System.out.print("Enter a number :");
+            int choice = input.nextInt();
+
+            if (choice == 1) {
+                // To display account information
+                accounts.get(UserIndex).AccountInformation();
+            } else if (choice == 2) {
+                Destination dis = new Destination();
+                dis.DisplayInf();
+            } else if (choice == 3) {
+                System.out.println("*****!!! Thank you for using Saudi Guide !!!*****");
+                System.out.println("           We hope you found the perfect spot you wanted <3 ");
+                System.out.println("                                SEE YA");
+                System.exit(0);
+            }
+
+        }
     }
 }
